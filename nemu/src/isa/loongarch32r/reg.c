a492eea37b995f30d85af906beaab309264cb1e2
tracer-ics2022 <tracer@njuics.org>
2023-05-22 21:24:32 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:24:32 up  2:09,  1 user,  load average: 0.42, 0.27, 0.26

--- nemu/src/isa/loongarch32r/reg.c
+++ nemu/src/isa/loongarch32r/reg.c
@@ -43,8 +43,8 @@ void isa_reg_display() {
     }
 
     printf("PC: 0x%08lx\n", cpu->pc);
-
-}
+    printf("liaoxue")
+} 
 
 word_t isa_reg_str2val(const char *s, bool *success) {
   return 0;
--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -20,7 +20,7 @@
 
 typedef struct {
   word_t gpr[32];
-  vaddr_t pc;
+  vaddr_t pc;    
 } riscv32_CPU_state;
 
 // decode
