4c04373d9d59564a10396e5f4d810f12041cea6e
tracer-ics2022 <tracer@njuics.org>
2023-09-18 19:55:21 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:55:21 up  4:31,  1 user,  load average: 0.29, 0.27, 0.23

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -32,12 +32,9 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-#define immJ() do { \
-  *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | \
-         (BITS(i, 30, 21) << 1) | \
-         (BITS(i, 20, 20) << 11) | \
-         (BITS(i, 19, 12) << 12); \
-} while(0)
+#define immJ() do { *imm = SEXT(BITS(i, 31, 31), 1) << 19 | ((SEXT(BITS(i, 19, 12), 8) << 56) >> 56) << 11 | ((SEXT(BITS(i, 20, 20), 1) << 63) >> 63) << 10 | (((SEXT(BITS(i, 30, 21), 10) << 54) >> 54) << 22) >> 22; *imm = *imm << 1; } while (0)
+#define immB() do { *imm = SEXT(BITS(i, 31, 31), 1) << 11 | ((SEXT(BITS(i, 7, 7), 1) << 63) >> 63) << 10 | ((SEXT(BITS(i, 30, 25), 6) << 58) >> 58) << 4 | ((SEXT(BITS(i, 11, 8), 4) << 60) >> 60); *imm = *imm << 1; } while (0)
+
 
 
 
@@ -110,7 +107,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi, I, R(rd) =src1 + imm);
 
     // 添加对 jal 指令的支持
-  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal, J,  R(rd) = s->snpc; s->dnpc = s->pc + imm);
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal, J,  R(rd) = s->snpc+4; s->dnpc = s->pc + imm-4);
   
   //无效指令
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
