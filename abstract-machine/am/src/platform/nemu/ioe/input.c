d9e469ced27552ccf04b24418d031b117cf2f7c6
tracer-ics2022 <tracer@njuics.org>
2023-10-23 15:11:48 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:11:48 up  4:57,  1 user,  load average: 0.24, 0.29, 0.24

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,9 +5,8 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  uint32_t kc = inl(KBD_ADDR);
-  kbd->keydown=kc & KEYDOWN_MASK ? true:false;
-  
-  //将键盘码中的按键按下状态标志位清除，只保留键值部分
-  kbd->keycode= kc& ~KEYDOWN_MASK;
+  int k = AM_KEY_NONE;
+  k = inl(KBD_ADDR);
+  kbd->keydown = (k & KEYDOWN_MASK ? true : false);
+  kbd->keycode = k & ~KEYDOWN_MASK;
 }
