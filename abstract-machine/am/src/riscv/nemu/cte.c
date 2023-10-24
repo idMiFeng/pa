382933b70af5c25a2b8296eda4dba44d5109af82
tracer-ics2022 <tracer@njuics.org>
2023-10-24 23:49:54 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:49:54 up  1:43,  1 user,  load average: 0.19, 0.27, 0.23

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,7 +8,7 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      default: ev.event = EVENT_ERROR; break;
+      //default: ev.event = EVENT_ERROR; break;
     }
 
     c = user_handler(ev, c);
