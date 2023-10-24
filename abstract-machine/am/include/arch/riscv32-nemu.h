d81ecd9fe91fcb51d7fdfef9615b306546271c00
tracer-ics2022 <tracer@njuics.org>
2023-10-24 23:42:18 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:42:18 up  1:35,  1 user,  load average: 0.33, 0.28, 0.21

--- abstract-machine/am/include/arch/riscv32-nemu.h
+++ abstract-machine/am/include/arch/riscv32-nemu.h
@@ -2,10 +2,15 @@
 #define ARCH_H__
 
 struct Context {
-  // TODO: fix the order of these members to match trap.S
-  uintptr_t mepc, mcause, gpr[32], mstatus;
+  // TODO: fix the order of these members to match trap.S 
+  //gpr为通用寄存器，mcause为csr异常号存储寄存器，mstatus为csr状态寄存器，mepc为异常地址寄存器
+  //通过观察trap.s中我们发现上下文存储的方式为先存储32个通用寄存器随后存储mcause mstatus mepc
+  uintptr_t gpr[32];
+  uintptr_t mcause, mstatus, mepc;
   void *pdir;
+  uintptr_t np;
 };
+
 
 #define GPR1 gpr[17] // a7
 #define GPR2 gpr[0]
