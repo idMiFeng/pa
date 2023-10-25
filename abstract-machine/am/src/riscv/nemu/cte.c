a76b58af71ca0c2893976c941850427cc24d8026
tracer-ics2022 <tracer@njuics.org>
2023-10-25 10:26:00 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:26:00 up 7 min,  1 user,  load average: 0.31, 0.35, 0.23

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,7 +7,9 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
+    printf("%"  "\n", c->mcause);
     switch (c->mcause) {
+      
       default: ev.event = EVENT_ERROR; break;
     }
     //user_handler是cte_init中注册的回调函数
