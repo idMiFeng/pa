1b768e9080d4dc152ecb3c3be0bd0ecd7e752c4f
tracer-ics2022 <tracer@njuics.org>
2023-10-29 22:27:16 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:27:16 up  4:17,  1 user,  load average: 0.74, 0.40, 0.28

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -83,15 +83,15 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count) {
  
-  // assert(fd == 1 || fd == 2);
+  assert(fd == 1 || fd == 2);
 
-  // assert(count > 0);
+  assert(count > 0);
   
-  // for (int i = 0; i < count; i++) {
-  //   putch(*((char*)buf + i));
-  // }
+  for (int i = 0; i < count; i++) {
+    putch(*((char*)buf + i));
+  }
 
-  // return count;
+  return count;
 }
 
 void *_sbrk(intptr_t increment) {
