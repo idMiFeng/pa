351eba4a119fd21f772baece506f49995ac51b9b
tracer-ics2022 <tracer@njuics.org>
2023-10-30 19:34:45 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:34:45 up  7:00,  1 user,  load average: 0.38, 0.29, 0.21

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -22,7 +22,7 @@
   SYS_times,
   SYS_gettimeofday
 };*/
-int SYS_write(intptr_t *buf, size_t count){
+SYS_write(intptr_t *buf, size_t count){
       for (int i = 0; i < count; i++) {
     putch(*((char*)buf + i));
   }
@@ -37,7 +37,7 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case 0:c->GPRx=0;printf("SYS_exit， do_syscall此时 c->GPRx=%d\n",c->GPRx);halt(c->GPRx);//SYS_exit系统调用
     case 1:printf("SYS_yield， do_syscall此时c->GPRx=%d\n",c->GPRx);yield(); //SYS_yield系统调用
-    case 4:int res=SYS_write(c->GPR2,c->GPR3);c->GPRx=res;return res;
+    case 4:SYS_write(c->GPR2,c->GPR3);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -81,7 +81,7 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count) {
   assert(fd == 1 || fd == 2);
-  //syscall_(SYS_write, (intptr_t)buf, count,0);
+  _syscall_(SYS_write, (intptr_t)buf, count,0);
   return (int)count;
   
 }
