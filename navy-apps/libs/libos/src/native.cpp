c8a463c0597c749a8fa8da5f8dff19c202bfa535
tracer-ics2022 <tracer@njuics.org>
2023-10-30 20:49:35 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:49:35 up  8:15,  1 user,  load average: 0.71, 0.32, 0.22

--- navy-apps/libs/libos/src/native.cpp
+++ navy-apps/libs/libos/src/native.cpp
@@ -231,7 +231,7 @@ ssize_t write(int fd, const void *buf, size_t count) {
     SDL_PauseAudio(0);
     return count;
   }
-  return glibc_write(fd, buf, count);
+  return count;
 }
 
 int execve(const char *filename, char *const argv[], char *const envp[]) {
