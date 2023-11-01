d52900e23b8be9f46c82b02f504eb51e609e50f4
tracer-ics2022 <tracer@njuics.org>
2023-11-01 21:41:41 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:41:41 up  4:42,  1 user,  load average: 0.25, 0.19, 0.18

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,27 @@
 #include <common.h>
 #include "syscall.h"
-
+/*enum {
+  SYS_exit,
+  SYS_yield,
+  SYS_open,
+  SYS_read,
+  SYS_write,
+  SYS_kill,
+  SYS_getpid,
+  SYS_close,
+  SYS_lseek,
+  SYS_brk,
+  SYS_fstat,
+  SYS_time,
+  SYS_signal,
+  SYS_execve,
+  SYS_fork,
+  SYS_link,
+  SYS_unlink,
+  SYS_wait,
+  SYS_times,
+  SYS_gettimeofday
+};*/
 int sys_write(intptr_t *buf, size_t count){
       for (int i = 0; i < count; i++) {
     putch(*((char*)buf + i));
