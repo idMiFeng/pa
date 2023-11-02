df941dfb6ee356da6490389e1a5279e726124e46
tracer-ics2022 <tracer@njuics.org>
2023-11-03 00:10:05 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:10:05 up  3:35,  1 user,  load average: 0.43, 0.46, 0.61

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -19,11 +19,23 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
   return len;
 }
 
+#define TEMP_BUF_SIZE 32
+static char temp_buf[TEMP_BUF_SIZE]; // for events_read
 size_t events_read(void *buf, size_t offset, size_t len) {
-    AM_INPUT_KEYBRD_T t = io_read(AM_INPUT_KEYBRD);
-   return snprintf((char *)buf, len, "%s %s\n", 
-    t.keydown ? "kd" : "ku",
-    keyname[t.keycode]);
+   memset(temp_buf, 0, TEMP_BUF_SIZE); // reset
+
+  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+  if (ev.keycode == AM_KEY_NONE) return 0;
+  const char *name = keyname[ev.keycode];
+  int ret = ev.keydown ? sprintf(temp_buf, "kd %s\n", name) : sprintf(temp_buf, "ku %s\n", name);
+  // ret -> exclude terminating null character
+  if (ret >= len) {
+      strncpy(buf, temp_buf, len - 1);
+      ret = len;
+  } else {
+      strncpy(buf, temp_buf, ret);
+  }
+  return ret; // ret -> include terminating null character
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
