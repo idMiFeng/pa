b8156aa9721308ac7ee13820538ed31cc8960326
tracer-ics2022 <tracer@njuics.org>
2023-11-04 23:21:14 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:21:14 up  7:14,  1 user,  load average: 0.90, 0.47, 0.32

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -134,9 +134,12 @@ static void init_dispinfo() {
 // 向画布`(x, y)`坐标处绘制`w*h`的矩形图像, 并将该绘制区域同步到屏幕上
 // 图像像素按行优先方式存储在`pixels`中, 每个像素用32位整数以`00RRGGBB`的方式描述颜色
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
+  printf("x,y=%d,%d\n",x,y);
   int fd = open("/dev/fb", 0, 0);
   printf("fd is %d\n",fd);
-  for (int i = 0; i < h && y + i < canvas_h; ++i) {
+  //一行一行写
+  for (int i = 0; i < h; ++i) {
+    //从文件开头偏移 offset 个字节。
     lseek(fd, (y + canvas_y + i) * screen_w + (x + canvas_x), SEEK_SET);
     write(fd, pixels + i * w, w < canvas_w - x ? w : canvas_w - x);
   }
