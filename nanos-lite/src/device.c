788014e53dec199b8105384bdb7ee8d791271909
tracer-ics2022 <tracer@njuics.org>
2023-11-05 13:36:08 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:36:08 up 4 min,  1 user,  load average: 1.05, 0.79, 0.35

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -40,13 +40,11 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 size_t fb_write(const void *buf, size_t offset, size_t len) {
  AM_GPU_CONFIG_T t = io_read(AM_GPU_CONFIG);
 
-  offset = offset / 4;
-  int w = len / 4;
 
   int y = offset / t.width;
   int x = offset - y * t.width;
 
-  io_write(AM_GPU_FBDRAW, x, y, (void*)buf, w, 1, true);
+  io_write(AM_GPU_FBDRAW, x, y, (void*)buf, len, 1, true);
   return len;
 } 
 
