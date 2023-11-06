103610f71dfd2d82ead76e70fa3a7a4df62c048f
tracer-ics2022 <tracer@njuics.org>
2023-11-07 00:20:40 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:20:40 up  5:31,  1 user,  load average: 0.37, 0.30, 0.32

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -130,8 +130,6 @@ size_t fs_lseek(int fd, size_t offset, int whence){
 void init_fs() {
   // TODO: initialize the size of /dev/fb
    AM_GPU_CONFIG_T ev = io_read(AM_GPU_CONFIG);
-  int width = ev.width;
-  int height = ev.height;
-  file_table[FD_FB].size = width * height * sizeof(uint32_t);
+  file_table[FD_FB].size = ev.vmemsz;
 
 }
