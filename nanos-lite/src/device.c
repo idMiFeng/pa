4c3cabb6d3ec89769b83053a496e1ff970a4165e
tracer-ics2022 <tracer@njuics.org>
2023-11-07 00:48:14 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:48:14 up  5:58,  1 user,  load average: 0.88, 0.51, 0.44

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -38,15 +38,17 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 
 //buf中的len字节写到屏幕上offset处
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  AM_GPU_CONFIG_T t = io_read(AM_GPU_CONFIG);
-  //offset = y * screen_width + x
-  //使用这个 offset 来在缓冲区中找到正确的数据，然后将其写入屏幕的 (x, y) 位置。
-  int y = offset / t.width;
-  int x = offset - y * t.width;
-  //len = h *screen_width + w
-  int h = len / t.width;
-  int w = len - h * t.width;
-  io_write(AM_GPU_FBDRAW, x, y, (void*)buf, w, h, true);
+  AM_GPU_CONFIG_T ev = io_read(AM_GPU_CONFIG);
+  int width = ev.width;
+
+  offset /= 4;
+  len /= 4;
+
+  int y = offset / width;
+  int x = offset - y * width;
+
+  io_write(AM_GPU_FBDRAW, x, y, (void *)buf, len, 1, true);
+
   return len;
 } 
 
