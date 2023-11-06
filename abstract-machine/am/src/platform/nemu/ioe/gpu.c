f472bdd2ab07ff0276b069e98e2475bb54731549
tracer-ics2022 <tracer@njuics.org>
2023-11-07 00:32:51 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:32:51 up  5:43,  1 user,  load average: 1.33, 0.77, 0.51

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -22,30 +22,19 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 #include <klib.h>
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  if (!ctl->sync && (w == 0 || h == 0)) return;
+  uint32_t *pixels = ctl->pixels;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-   //uint16_t W = (inl(VGACTL_ADDR) & 0xff00) >> 16;
-   //uint16_t H = inl(VGACTL_ADDR) & 0x00ff;
-   uint32_t screen_wh = inl(VGACTL_ADDR);
-  uint32_t H = screen_wh & 0xffff;
-  uint32_t W = screen_wh >> 16;
-
-  int x = ctl->x;
-  int y = ctl->y;
-  int w = ctl->w;
-  int h = ctl->h;
-  uint32_t * base = (uint32_t *) ctl->pixels;
-  int cp_bytes = sizeof(uint32_t) * (w < W - x ? w : W - x);
-  for (int j = 0; j < h && y + j < H; ++j) {
-      memcpy(&fb[(y + j) * W + x], base, cp_bytes);
-      base += w;
+  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
+  for (int i = y; i < y+h; i++) {
+    for (int j = x; j < x+w; j++) {
+      fb[screen_w*i+j] = pixels[w*(i-y)+(j-x)];
+    }
   }
-  //for (int j = 0; j < h; ++j)
-  //  for (int i = 0; i < w; ++i)
-  //    fb[(y + j) * W + x + i] = base[j * w + i];
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
-  // putstr("__am_gpu_fbdraw\n");
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
