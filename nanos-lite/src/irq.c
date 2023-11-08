aa4a82498cd0e2e1586080623aa1521cb5e59581
tracer-ics2022 <tracer@njuics.org>
2023-11-08 17:15:21 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:15:21 up  6:46,  1 user,  load average: 0.29, 0.24, 0.26

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -7,8 +7,9 @@ static Context* do_event(Event e, Context* c) {
    switch (e.event) {
 
     case EVENT_YIELD:
-        Log("EVENT_YIELD");
+        Log("schedule %x\n",c->mepc);
         c = schedule(c);
+        Log("schedule之后 %x\n",c->mepc);
         break;
     case EVENT_SYSCALL:do_syscall(c);break;//EVENT_SYSCALL
     default: panic("Unhandled event ID = %d", e.event);break;
