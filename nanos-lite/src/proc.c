8cea5fa685fad1ade2f6e0ebdea82c68e7742c6b
tracer-ics2022 <tracer@njuics.org>
2023-11-08 22:33:28 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:33:28 up 11:00,  1 user,  load average: 0.20, 0.29, 0.20

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -13,7 +13,7 @@ void switch_boot_pcb() {
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
+   Log("Hello World from Nanos-lite with arg '%s' for the %dth time!", (const char *)arg, j);
     j ++;
     yield();
   }
@@ -27,7 +27,8 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
 }
 
 void init_proc() {
-  context_kload(&pcb[0], hello_fun, NULL);//把 hello_fun()函数的地址作为entry传入
+  context_kload(&pcb[0], hello_fun, "A");
+  context_kload(&pcb[1], hello_fun, "B");
   switch_boot_pcb();
 
   Log("Initializing processes...");
@@ -43,7 +44,7 @@ Context* schedule(Context *prev) {
   current->cp = prev;
  
 // always select pcb[0] as the new process
-  current = &pcb[0];
+   current = ((current == &pcb[0]) ? &pcb[1] : &pcb[0]);
   
 // then return the new context
   return current->cp;
