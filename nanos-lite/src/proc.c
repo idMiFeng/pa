ccbb39aad25b94dc00abbd2a06aeb327b0582279
tracer-ics2022 <tracer@njuics.org>
2023-11-08 17:18:17 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:18:17 up  6:49,  1 user,  load average: 0.25, 0.27, 0.27

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -39,7 +39,7 @@ void init_proc() {
 
 Context* schedule(Context *prev) {
   // save the context pointer
-  current->cp = prev;
+ pcb[3].cp = prev;
 
 // always select pcb[0] as the new process
   current = &pcb[0];
