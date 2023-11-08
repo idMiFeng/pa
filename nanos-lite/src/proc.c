57b9cd8891b887d33a0ff2d9733a760fe5894a28
tracer-ics2022 <tracer@njuics.org>
2023-11-08 18:09:24 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:09:24 up  7:40,  1 user,  load average: 0.34, 0.20, 0.21

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -12,11 +12,11 @@ void switch_boot_pcb() {
 
 void hello_fun(void *arg) {
   int j = 1;
- 
+  while (1) {
     Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
     j ++;
     yield();
-  
+  }
 }
 
 void context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
