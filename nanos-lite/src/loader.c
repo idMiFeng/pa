9e79e8d42eddd191d1e92e1aed7a4329723d98d5
fengmi <1498481442@qq.com>
2023-11-11 01:29:31 +0800

    last

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -54,17 +54,81 @@ void naive_uload(PCB *pcb, const char *filename) {
   ((void(*)())entry) ();
 }
 
-void context_uload(PCB *pcb, const char *filename) {
-   Area stack;
-  stack.start = pcb->stack;
-  stack.end = pcb->stack + STACK_SIZE;
-  uintptr_t entry = loader(pcb, filename);
 
-  pcb->cp = ucontext(NULL, stack, (void*)entry);
-  pcb->cp->GPRx = (uintptr_t) heap.end;
+void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]) {
+    // 定义用户栈的区域
+    Area stack;
+    stack.start = pcb->stack;
+    stack.end = pcb->stack + STACK_SIZE;
+
+    // 调用 loader 函数加载用户程序，获取入口地址
+    uintptr_t entry = loader(pcb, filename);
+
+    // 计算 argc、envc 的值
+    int argc = 0;
+    while (argv[argc] != NULL) argc++;
+    int envc = 0;
+    while (envp[envc] != NULL) envc++;
+
+    // 分配用户栈空间，用于存储 argv 和 envp 指针
+    uintptr_t* user_stack = (uintptr_t*)heap.end;
+
+    // 将 argv 字符串逆序拷贝到用户栈
+    for (int i = argc - 1; i >= 0; i--) {
+        size_t len = strlen(argv[i]) + 1;  // 包括 null 终止符
+        user_stack -= len;
+        strncpy((char*)user_stack, argv[i], len);
+    }
+
+    // 对齐到 uintptr_t 边界
+    user_stack = (uintptr_t*)((uintptr_t)user_stack & ~(sizeof(uintptr_t) - 1));
+
+    // 将 envp 字符串逆序拷贝到用户栈
+    for (int i = envc - 1; i >= 0; i--) {
+        size_t len = strlen(envp[i]) + 1;  // 包括 null 终止符
+        user_stack -= len;
+        strncpy((char*)user_stack, envp[i], len);
+    }
+
+    // 对齐到 uintptr_t 边界
+    user_stack = (uintptr_t*)((uintptr_t)user_stack & ~(sizeof(uintptr_t) - 1));
+
+    // 将 argv 和 envp 指针拷贝到用户栈
+    user_stack -= (argc + envc + 4);  // +4 为 NULL 结尾和 argc/envc 的值
+    uintptr_t* user_argv = user_stack;
+
+    // 设置 argc 的值
+    user_stack[0] = argc;
+
+    // 设置 argv 指针
+    for (int i = 0; i < argc; i++) {
+        user_stack[i + 1] = (uintptr_t)heap.end - (argc - i - 1) * sizeof(uintptr_t);
+    }
+
+    // 设置 argv 的 NULL 终止符
+    user_stack[argc + 1] = 0;
+
+    // 设置 envc 的值
+    user_stack[argc + 2] = envc;
+
+    // 设置 envp 指针
+    for (int i = 0; i < envc; i++) {
+        user_stack[argc + 3 + i] = (uintptr_t)heap.end - (argc + 3 + envc - i - 1) * sizeof(uintptr_t);
+    }
+
+    // 设置 envp 的 NULL 终止符
+    user_stack[argc + 3 + envc] = 0;
+
+    // 调用 ucontext 函数创建用户上下文，传入入口地址和用户栈
+    pcb->cp = ucontext(pcb->cp, stack, (void*)entry);
+
+    // 将用户栈的顶部地址赋给 GPRx 寄存器
+    pcb->cp->GPRx = (uintptr_t)user_stack;
+}
+
   /*Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
   Context *uctx = (Context *)(kstack.end-sizeof(Context));
   uctx->mepc=(uintptr_t) entry;
   return uctx;
 }*/
-}
+
--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,8 +5,19 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  char *empty[] =  {NULL };
-  environ = empty;
-  exit(main(0, empty, empty));
+   // 从参数中获取argc的值
+  int argc = (int)args[0];
+  
+  // 从参数中获取argv的地址
+  char **argv = (char **)(args + 1);
+
+  // 从参数中获取envp的地址
+  char **envp = (char **)(args + argc + 1);
+
+  // 设置environ为envp
+  environ = envp;
+
+  // 调用main函数
+  exit(main(argc, argv, envp));
   assert(0);
 }
