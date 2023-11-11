05bf1db88f36e8576abd17b072f643bb7f789146
tracer-ics2022 <tracer@njuics.org>
2023-11-11 21:15:20 +0800

    >  compile NEMU 211220000 张三 Linux fengmi 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:15:20 up 21 min,  1 user,  load average: 0.29, 0.34, 0.28

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,14 +5,19 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  uintptr_t *base = args;
-  int argc = *base;
-  base += 1;
-  char **argv = (char **)base;
-  // for (int i = 0; i < argc; ++i) printf("argv[%d]: %s\n", i, argv[i]);
-  base += (argc + 1);
-  char **envp = (char **)base;
+   // 从参数中获取argc的值
+  int argc = (int)args[0];
+  
+  // 从参数中获取argv的地址
+  char **argv = (char **)(args + 1);
+
+  // 从参数中获取envp的地址
+  char **envp = (char **)(args + argc + 1);
+
+  // 设置environ为envp
   environ = envp;
+
+  // 调用main函数
   exit(main(argc, argv, envp));
   assert(0);
 }
